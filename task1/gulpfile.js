665e858ddb9d5f1ff49f350ca9db56df58294318
Viktor_Kuzeminskyi <Vkuzeminskyi@copyright.com>
2016-03-11 17:46:22 +0200

    added images,fiew gulp tasks,template

--- task1/gulpfile.js
+++ task1/gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var watch = require("gulp-watch");
 var sourcemaps = require("gulp-sourcemaps");
 var autoprefixer = require("gulp-autoprefixer");
 var cssmin = require("gulp-minify-css");
 var uglify = require("gulp-uglify");
-var gulpif = require("gulp-if");
-var del = require("del");
-var cssnano = require('gulp-cssnano'),
+var inject = require('gulp-inject');
+var jade = require('gulp-jade');
+var
     //jshint = require('gulp-jshint'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
@@ -17,18 +16,20 @@ var cssnano = require('gulp-cssnano'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload');
 
-
 var path = {
     src: {
         less:'src/styl/**/*',
         images: 'src/img/**/*',
         css: 'src/styl/all.less',
         js: 'src/js/**/*',
+        mainJade:"src/templates/index.jade",
+        templates: "src/templates/**/*",
         html:'src/index.html'
     },
     build: {
-        css: 'build/css',
-        img: 'build/img'
+        css: './build/css',
+        img: './build/img',
+        html:'./build/index.html'
     }
 };
 
@@ -36,9 +37,9 @@ gulp.task('images', function () {
     return gulp.src(path.src.images)
         .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
         .pipe(gulp.dest(path.build.img))
-
-
+        .pipe(livereload());
 });
+
 gulp.task('styles',function(){
    return gulp.src(path.src.css)
        .pipe(less())
@@ -46,26 +47,39 @@ gulp.task('styles',function(){
        .pipe(cssmin())
        .pipe(rename({suffix: '.min'}))
        .pipe(gulp.dest(path.build.css))
+       .pipe(livereload());
 });
+
 gulp.task('html',function(){
-   return gulp.src(path.src.html)
+    return gulp.src( path.src.mainJade)
+       .pipe(jade({pretty: true}))
        .pipe(gulp.dest('build'))
+       .pipe(livereload());
+});
+
+gulp.task('html:inject',function(){
+
+    var sources = gulp.src(['build/css/*'], {read: false});
+
+    return gulp.src(path.build.html)
+        .pipe(inject(sources),{})
+        .pipe(gulp.dest('build'))
+        .pipe(livereload());
 });
 
 
-gulp.task('build',function(){
-   gulp.start('images','styles','html');
+gulp.task('build',['images','styles','html'],function(){
+    gulp.start('html:inject')
 });
+
 gulp.task('watch',function(){
-    watch(path.src.less, function(event, cb) {
+    watch(path.src.less, function(){
         gulp.start('styles');
     });
-
-    watch(path.src.images, function(event, cb) {
+    watch(path.src.images, function(){
         gulp.start('images');
     });
-    watch(path.src.html,function(event,cb){
+    watch(path.src.templates,function(){
         gulp.start('html');
     });
-
 });
